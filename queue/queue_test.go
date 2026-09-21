67b26b9b3a0543047356ad2dd18442aee3c7b685
shreyasardar <b24cm1056@iitj.ac.in>
2026-09-21 23:29:22 +0530

    Add concurrent enqueue dequeue test

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -29,3 +29,39 @@ func TestConcurrentEnqueue(t *testing.T) {
 		t.Errorf("expected 100 messages, got %d", q.Size())
 	}
 }
+
+func TestConcurrentEnqueueDequeue(t *testing.T) {
+	q := NewQueue()
+
+	var wg sync.WaitGroup
+
+	// 100 producers
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			q.Enqueue(models.Message{
+				Value: "test",
+			})
+		}()
+	}
+
+	wg.Wait()
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			q.Dequeue()
+		}()
+	}
+
+	wg.Wait()
+
+	if !q.IsEmpty() {
+		t.Errorf("expected queue to be empty, got %d messages", q.Size())
+	}
+}
